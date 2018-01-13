8130308fb3da623ce3cebd3dc5be515517e5d00a
Louis Pilfold <louis@lpil.uk>
2018-01-13 16:52:15 +0000

    Extract params from parsed query

--- lib/yesql.ex
+++ lib/yesql.ex
@@ -6,13 +6,22 @@ defmodule Yesql do
   @doc false
   def parse(sql) do
     with {:ok, tokens, _} <- Yesql.Tokenizer.tokenize(sql) do
-      output =
+      {query_iodata, rev_params} =
         tokens
         |> Enum.chunk_by(&elem(&1, 0))
         |> Enum.flat_map(&merge_fragments/1)
+        |> Enum.reduce({[], []}, &extract_param/2)
 
-      {:ok, output}
+      {:ok, IO.iodata_to_binary(query_iodata), Enum.reverse(rev_params)}
     end
+  end
+
+  defp extract_param(param, {sql, params}) when is_atom(param) do
+    {[sql, "?"], [param | params]}
+  end
+
+  defp extract_param(fragment, {sql, params}) do
+    {[sql, fragment], params}
   end
 
   defp merge_fragments([{:fragment, _} | _] = tokens) do
--- test/yesql_test.exs
+++ test/yesql_test.exs
@@ -3,76 +3,70 @@ defmodule YesqlTest do
   doctest Yesql
 
   describe "parse/1" do
-    def input >>> output do
-      assert Yesql.parse(input) == {:ok, output}
-    end
+    import Yesql, only: [parse: 1]
 
     test "simple tests" do
-      "SELECT * FROM person WHERE age > 18" >>> ["SELECT * FROM person WHERE age > 18"]
-      "SELECT * FROM person WHERE age > :age" >>> ["SELECT * FROM person WHERE age > ", :age]
-      "SELECT * FROM person WHERE :age > age" >>> ["SELECT * FROM person WHERE ", :age, " > age"]
-      "SELECT 1 FROM dual" >>> ["SELECT 1 FROM dual"]
-      "SELECT :value FROM dual" >>> ["SELECT ", :value, " FROM dual"]
-      "SELECT 'test' FROM dual" >>> ["SELECT 'test' FROM dual"]
-      "SELECT 'test'\nFROM dual" >>> ["SELECT 'test'\nFROM dual"]
+      assert parse("SELECT * FROM person WHERE age > 18") ==
+               {:ok, "SELECT * FROM person WHERE age > 18", []}
 
-      "SELECT :value, :other_value FROM dual" >>>
-        ["SELECT ", :value, ", ", :other_value, " FROM dual"]
+      assert parse("SELECT * FROM person WHERE age > :age") ==
+               {:ok, "SELECT * FROM person WHERE age > ?", [:age]}
+
+      assert parse("SELECT * FROM person WHERE :age > age") ==
+               {:ok, "SELECT * FROM person WHERE ? > age", [:age]}
+
+      assert parse("SELECT 1 FROM dual") == {:ok, "SELECT 1 FROM dual", []}
+      assert parse("SELECT :value FROM dual") == {:ok, "SELECT ? FROM dual", [:value]}
+      assert parse("SELECT 'test' FROM dual") == {:ok, "SELECT 'test' FROM dual", []}
+      assert parse("SELECT 'test'\nFROM dual") == {:ok, "SELECT 'test'\nFROM dual", []}
+
+      assert parse("SELECT :value, :other_value FROM dual") ==
+               {:ok, "SELECT ?, ? FROM dual", [:value, :other_value]}
     end
 
     test "Tokenization rules" do
-      "SELECT :age-5 FROM dual" >>> ["SELECT ", :age, "-5 FROM dual"]
+      assert parse("SELECT :age-5 FROM dual") == {:ok, "SELECT ?-5 FROM dual", [:age]}
     end
 
     test "escapes" do
-      "SELECT :value, :other_value, ':not_a_value' FROM dual" >>>
-        ["SELECT ", :value, ", ", :other_value, ", ':not_a_value' FROM dual"]
+      assert parse("SELECT :value, :other_value, ':not_a_value' FROM dual") ==
+               {:ok, "SELECT ?, ?, ':not_a_value' FROM dual", [:value, :other_value]}
 
-      ~S"SELECT 'not \' :a_value' FROM dual" >>> [~S"SELECT 'not \' :a_value' FROM dual"]
+      assert parse(~S"SELECT 'not \' :a_value' FROM dual") ==
+               {:ok, ~S"SELECT 'not \' :a_value' FROM dual", []}
     end
 
     test "casting" do
-      "SELECT :value, :other_value, 5::text FROM dual" >>>
-        ["SELECT ", :value, ", ", :other_value, ", 5::text FROM dual"]
+      assert parse("SELECT :value, :other_value, 5::text FROM dual") ==
+               {:ok, "SELECT ?, ?, 5::text FROM dual", [:value, :other_value]}
     end
 
     test "newlines are preserved" do
-      "SELECT :value, :other_value, 5::text\nFROM dual" >>>
-        ["SELECT ", :value, ", ", :other_value, ", 5::text\nFROM dual"]
+      assert parse("SELECT :value, :other_value, 5::text\nFROM dual") ==
+               {:ok, "SELECT ?, ?, 5::text\nFROM dual", [:value, :other_value]}
     end
 
     test "complex 1" do
-      "SELECT :a+2*:b+age::int FROM users WHERE username = :name AND :b > 0" >>>
-        [
-          "SELECT ",
-          :a,
-          "+2*",
-          :b,
-          "+age::int FROM users WHERE username = ",
-          :name,
-          " AND ",
-          :b,
-          " > 0"
-        ]
+      assert parse("SELECT :a+2*:b+age::int FROM users WHERE username = :name AND :b > 0") ==
+               {
+                 :ok,
+                 "SELECT ?+2*?+age::int FROM users WHERE username = ? AND ? > 0",
+                 [:a, :b, :name, :b]
+               }
     end
 
     test "complex 2" do
-      "SELECT :value1 + :value2 + value3 + :value4 + :value1\nFROM SYSIBM.SYSDUMMY1" >>>
-        [
-          "SELECT ",
-          :value1,
-          " + ",
-          :value2,
-          " + value3 + ",
-          :value4,
-          " + ",
-          :value1,
-          "\nFROM SYSIBM.SYSDUMMY1"
-        ]
+      assert parse("SELECT :value1 + :value2 + value3 + :value4 + :value1\nFROM SYSIBM.SYSDUMMY1") ==
+               {
+                 :ok,
+                 "SELECT ? + ? + value3 + ? + ?\nFROM SYSIBM.SYSDUMMY1",
+                 [:value1, :value2, :value4, :value1]
+               }
     end
 
     test "complex 3" do
-      "SELECT ARRAY [:value1] FROM dual" >>> ["SELECT ARRAY [", :value1, "] FROM dual"]
+      assert parse("SELECT ARRAY [:value1] FROM dual") ==
+               {:ok, "SELECT ARRAY [?] FROM dual", [:value1]}
     end
   end
 end
