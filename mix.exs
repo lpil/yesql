dd0a8f6284b4bc6fbaa14cd3f9489ef5ce0ead6b
Louis Pilfold <louis@lpil.uk>
2018-01-20 13:53:10 +0000

    v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,25 +4,33 @@ defmodule Yesql.Mixfile do
   def project do
     [
       app: :yesql,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Yesql",
+      description: "Using plain old SQL to query databases",
+      package: [
+        maintainers: ["Louis Pilfold"],
+        licenses: ["Apache 2.0"],
+        links: %{"GitHub" => "https://github.com/lpil/yesql"},
+        files: ~w(LICENCE README.md lib src/Elixir.Yesql.Tokenizer.xrl mix.exs)
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    [extra_applications: []]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      # Postgresql driver
+      {:postgrex, "~> 0.12", optional: true},
+      # Automatic testing tool
       {:mix_test_watch, ">= 0.0.0", only: :dev},
-      {:postgrex, "~> 0.12", only: [:dev, :test]},
+      # Documentation generator
+      {:ex_doc, "~> 0.18.0", only: :dev}
     ]
   end
 end
